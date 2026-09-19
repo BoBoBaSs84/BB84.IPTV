156eb2857feb4836a79b206f03adb54f8df040ff
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2026-09-19 10:12:14 +0200

    feat: use source generator for EntityBase equality
    add [GenerateEquality] to EntityBase, remove manual IEquatable, Equals, GetHashCode

--- src/BB84.IPTV.M3U.Editor.Domain/Entities/Base/EntityBase.cs
+++ src/BB84.IPTV.M3U.Editor.Domain/Entities/Base/EntityBase.cs
@@ -1,4 +1,5 @@
 ﻿using BB84.EntityFrameworkCore.Entities;
+using BB84.SourceGenerators.Attributes;
 
 namespace BB84.IPTV.M3U.Editor.Domain.Entities.Base;
 
@@ -6,17 +7,5 @@ namespace BB84.IPTV.M3U.Editor.Domain.Entities.Base;
 /// Represents the base class for all entities in the application, providing a
 /// common identifier property and equality comparison based on the identifier.
 /// </summary>
-public abstract class EntityBase : IdentityEntity<int>, IEquatable<EntityBase>
-{
-	/// <inheritdoc/>
-	public bool Equals(EntityBase? other)
-		=> other is not null && Id == other.Id;
-
-	/// <inheritdoc/>
-	public override bool Equals(object? obj)
-		=> obj is EntityBase other && Equals(other);
-
-	/// <inheritdoc/>
-	public override int GetHashCode()
-		=> base.GetHashCode();
-}
+[GenerateEquality]
+public abstract partial class EntityBase : IdentityEntity<int>;
